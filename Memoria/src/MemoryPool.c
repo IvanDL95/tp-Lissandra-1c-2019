caaf1a8766ba01c45a8f494db77e35ab45f1f0b5
IvanDL95 <ivandeluca.95@gmail.com>
2019-05-22 23:55:31 -0300

    Memoria realiza la función SELECT

--- Memoria/src/MemoryPool.c
+++ Memoria/src/MemoryPool.c
@@ -23,8 +23,9 @@ void inicializar_memoria();
 void iniciar_gossiping();
 int conectarse_con_FS();
 void iniciar_servidor2();
-void crear_nuevo_segmento(char*);
-void segmento_unico_checkpoint2();
+void crear_nuevo_segmento(const char*);
+char* buscar_key(tabla_paginas, int key);
+static void solicitar_pagina(const char* value);
 
 int main(int argc, char** argv){
 	char* pathMemoriaConfig = argv[1];
@@ -33,7 +34,7 @@ int main(int argc, char** argv){
 
 	get_configuracion(pathMemoriaConfig);
 
-	switch(conectarse_con_FS()){
+	/*switch(conectarse_con_FS()){
 		case cop_ok:
 			break;
 		case -1:
@@ -45,17 +46,14 @@ int main(int argc, char** argv){
 			enviar(socket_FS,codigo_error,0,NULL);
 			exit(EXIT_FAILURE);
 			break;
-	}
+	}*/
 
 	inicializar_memoria();
 	log_info(logger, "Memoria Principal reservada\n");
 
-	segmento_unico_checkpoint2();
+	crear_nuevo_segmento("Tabla_prueba");
 	log_info(logger, "Segmento unico checkpoint 2 creado\n");
 
-
-	pthread_create(&hilo_gossiping, NULL, (void*) iniciar_gossiping, NULL);
-	log_debug(logger, "Gossiping inicializado\n");
 	/*
 	t_gossip* memory[2];
 
@@ -69,6 +67,9 @@ int main(int argc, char** argv){
 	pthread_create(&hilo_consola, NULL, (void*) iniciar_consola, logger);
 
 	pthread_create(&hilo_server, NULL, (void*) iniciar_servidor2, NULL);
+
+	pthread_create(&hilo_gossiping, NULL, (void*) iniciar_gossiping, NULL);
+	log_debug(logger, "Gossiping inicializado\n");
 
 	pthread_join(hilo_consola, NULL);
 	pthread_join(hilo_server, NULL);
@@ -102,6 +103,111 @@ void get_configuracion(char* ruta){
 	config_destroy(archivo_configuracion);
 }
 
+int ejecutar_API(command_api operacion, char** argumento){
+	log_debug(logger, "Ejecutando la API\n");
+	// K&R = D&D
+	char* nombre_tabla;
+	char* value = malloc(tamanio_value);
+	int key;
+
+	switch(operacion){
+		case SELECT:
+			log_debug(logger, "SELECT %s %s\n", argumento[0], argumento[1]);
+			nombre_tabla = malloc(size_of_string(argumento[0]));
+			strcpy(nombre_tabla,argumento[0]);
+			key = atoi(argumento[1]);
+
+			int _is_equal_segmento(t_segmento* segmento){
+				// la lógica del list_find está al revés
+				return !strcmp(nombre_tabla,segmento->nombre_tabla);
+			}
+
+			t_segmento* segmento_buscado = (t_segmento*)list_find(tabla_segmentos,(void*)_is_equal_segmento);
+
+			//Si el segmento existe ejecuta el if
+			if(segmento_buscado != NULL){
+				char* new_value = malloc(tamanio_value);
+				new_value = buscar_key(segmento_buscado->tabla, key);
+				if(new_value != NULL)
+					//Si encuentra la key devuelve su value y la retorna, sino sale del if y continua
+					return new_value;
+			}
+			t_list* lista_strings = list_create();
+			// for(int i=0; argumento[i] != NULL;i++)
+				list_add(lista_strings,argumento[0]);
+				list_add(lista_strings,argumento[1]);
+			enviar_listado_de_strings(socket_FS,lista_strings,SELECT);
+			t_paquete* paquete_recibido = recibir(socket_FS);
+
+			int desplazamiento = 0;
+			char* new_value = deserializar_string(paquete_recibido->data,&desplazamiento /* 0 */ );
+			solicitar_pagina(new_value);
+			return new_value;
+
+			break;
+		case INSERT:
+			log_debug(logger, "INSERT %s %s\n", argumento[0], argumento[1], argumento[2]);
+			nombre_tabla = malloc(size_of_string(argumento[0]));
+			strcpy(nombre_tabla,argumento[0]);
+			strcpy(value,argumento[2]);
+			key = atoi(argumento[1]);
+
+			break;
+		case CREATE:
+			printf("hacer CREATE\n");
+			break;
+		case DESCRIBE:
+			printf("hacer DESCRIBE\n");
+			break;
+		case DROP:
+			printf("hacer DROP\n");
+			break;
+		case JOURNAL:
+			printf("hacer JOURNAL\n");
+			break;
+		default:
+			log_info(logger,"Paquete no reconocido\n");
+	}
+	return 0;
+}
+
+
+/****************** FUNCIONES DE MEMORIA **************************/
+
+void inicializar_memoria(){
+	int tamanio_pagina = tamanio_base_pagina + tamanio_value;
+	unsigned int cantindad_frames = config_MP.TAM_MEM/tamanio_pagina;
+	memoria_principal = calloc(cantindad_frames,tamanio_pagina);
+	log_debug(logger, "Malloc memoria exitoso\n");
+
+	tabla_segmentos = list_create();
+}
+
+void crear_nuevo_segmento(const char* nombre_tabla){
+	t_segmento* segmento_nuevo = malloc(sizeof(t_segmento));
+
+	segmento_nuevo->nombre_tabla = malloc(size_of_string(nombre_tabla));
+	strcpy(segmento_nuevo->nombre_tabla, nombre_tabla);
+	//segmento_nuevo.nombre_tabla = nombre_tabla;
+	log_debug(logger, "Tabla: %s\n",segmento_nuevo->nombre_tabla);
+	segmento_nuevo->tabla = list_create();
+
+
+	list_add(tabla_segmentos,segmento_nuevo);
+	log_debug(logger, "Nuevo segmento creado\n");
+}
+
+char* buscar_key(tabla_paginas tabla, int key){
+	log_info(logger,"buscando key\n");
+	return "0";
+}
+
+static void solicitar_pagina(const char* valor){
+	log_info(logger,"Solcitando página para value: %s\n", valor);
+}
+
+/****************** CONEXIONES **************************/
+
 int conectarse_con_FS(){
 	un_socket socket_recibido;
 	do socket_recibido = conectar_a(config_MP.IP_FS, config_MP.PUERTO_FS);
@@ -124,6 +230,7 @@ int conectarse_con_FS(){
 	liberar_paquete(paquete_recibido);
 	return cop_ok;
 }
+
 
 static void administrar_conexion(un_socket nuevo_socket){
 	t_paquete* paquete_recibido;
@@ -159,56 +266,7 @@ static void administrar_conexion(un_socket nuevo_socket){
 	}
 }
 
-int ejecutar_API(command_api operacion, char** argumentos){
-	log_debug(logger, "Ejecutando la API\n");
-	switch(operacion){
-		case SELECT:
-			log_debug(logger, "SELECT %s %s\n", argumentos[0], argumentos[1]);
-			printf("hacer SELECT\n");
-			break;
-		case INSERT:
-			log_debug(logger, "INSERT %s %s\n", argumentos[0], argumentos[1]);
-			printf("hacer INSERT\n");
-			break;
-		case CREATE:
-			printf("hacer CREATE\n");
-			break;
-		case DESCRIBE:
-			printf("hacer DESCRIBE\n");
-			break;
-		case DROP:
-			printf("hacer DROP\n");
-			break;
-		case JOURNAL:
-			printf("hacer JOURNAL\n");
-			break;
-		default:
-			log_info(logger,"Paquete no reconocido\n");
-	}
-	return 0;
-}
-
-void inicializar_memoria(){
-	int tamanio_pagina = tamanio_base_pagina + tamanio_value;
-	unsigned int cantindad_frames = config_MP.TAM_MEM/tamanio_pagina;
-	memoria_principal = calloc(cantindad_frames,tamanio_pagina);
-	log_debug(logger, "Malloc memoria exitoso\n");
-
-	tabla_segmentos = list_create();
-}
-
-void segmento_unico_checkpoint2(){
-	crear_nuevo_segmento("Tabla_prueba");
-}
-
-void crear_nuevo_segmento(char* nombre_tabla){
-	t_segmento segmento_nuevo;
-	segmento_nuevo.nombre_tabla = malloc(size_of_string(nombre_tabla));
-	strcpy(segmento_nuevo.nombre_tabla, nombre_tabla);
-	//segmento_nuevo.nombre_tabla = nombre_tabla;
-	segmento_nuevo.tabla = list_create();
-	log_debug(logger, "Nuevo segmento creado\n");
-}
+// Cambiar a otro archivo 'global' ???
 
 void iniciar_servidor2(){
 	un_socket socket_listener = socket_escucha(IP,config_MP.PUERTO_ESCUCHA);
