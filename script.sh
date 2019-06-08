a047ca010b8ee802ab3c0678bbb70ad0978ce29f
alexjgonzalez <alex@sceu.frba.utn.edu.ar>
2019-06-08 10:06:49 -0300

    ajustes en script

--- script.sh
+++ script.sh
@@ -7,10 +7,10 @@ sudo make
 
 sudo makeinstall
 
-sudo git clone https://github.com/sisoputnfrba/tp-2019-1c-La-kernel-de-los-s-bados.git
+git clone https://github.com/sisoputnfrba/tp-2019-1c-La-kernel-de-los-s-bados.git
 
 
-cd home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/Debug
+cd /home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/Debug
 
 sudo makefile
 
@@ -18,16 +18,16 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/tp-2019-1c-La-kernel-de-los-
 
 cd /home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Lissandra FileSystem/src
 
-sudo gcc -I/home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/ -L/home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/Debug/ Filesystem.c Lissandra.c /home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/ClientServer.c -Wl,-rpath=/home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/Debug/ -o Filesystem -lcommons -lLibraries -lreadline -lpthread
+gcc -I/home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/ -L/home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/Debug/ Filesystem.c Lissandra.c /home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/ClientServer.c -Wl,-rpath=/home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/Debug/ -o Filesystem -lcommons -lLibraries -lreadline -lpthread
 
 cd /home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Memoria/src
 
-sudo gcc -I/home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/ -L/home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/Debug/ MemoryPool.c /home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/ClientServer.c -Wl,-rpath=/home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/Debug/ -o Memoria -lcommons -lLibraries -lreadline -lpthread
+gcc -I/home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/ -L/home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/Debug/ MemoryPool.c /home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/ClientServer.c -Wl,-rpath=/home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/Debug/ -o Memoria -lcommons -lLibraries -lreadline -lpthread
 
 
 cd /home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Kernel/src
 
 
-sudo gcc -I/home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/ -L/home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/Debug/ Kernel.c /home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/ClientServer.c -Wl,-rpath=/home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/Debug/ -o Kernel -lcommons -lLibraries -lreadline -lpthread
+gcc -I/home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/ -L/home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/Debug/ Kernel.c /home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/ClientServer.c -Wl,-rpath=/home/utnso/tp-2019-1c-La-kernel-de-los-s-bados/Libraries/Debug/ -o Kernel -lcommons -lLibraries -lreadline -lpthread
 
 
