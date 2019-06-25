b1ae2b9d93a0b0c3ced4b38de6d2ee4a2ad19a20
alexjgonzalez <alex@sceu.frba.utn.edu.ar>
2019-06-25 16:34:32 -0300

    archivo con variables globales

---
+++ Kernel/src/Globals.h
@@ -0,0 +1,20 @@
+/*
+ * Globals.h
+ *
+ *  Created on: 17 jun. 2019
+ *      Author: utnso
+ */
+
+#ifndef SRC_GLOBALS_H_
+#define SRC_GLOBALS_H_
+
+#include <pthread.h>
+
+pthread_t thread_planificador;
+
+Configuracion_Kernel config_Kernel;
+
+un_socket socket_Memoria = 0;
+
+
+#endif /* SRC_GLOBALS_H_ */
